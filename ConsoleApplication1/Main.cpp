ab12ce96a81973d0735e64bf50b4b33435cc1762
Alyssa Barbee <alb3dk@umsystem.edu>
2024-12-05 13:57:28 -0600

    Update

--- ConsoleApplication1/Main.cpp
+++ ConsoleApplication1/Main.cpp
@@ -18,7 +18,7 @@ int main() {
         }; 
 
         for (int i = 0; i < myExpressions.size(); ++i){
-            evaluate(myExpressions[i]);
+            infixEvaluator(myExpressions[i]);
         }
 
     }
